0cbc88997cf2d0da082bdfe8d0caed0d4d2409a0
Stanislav_Ksenzov <daxxon@mail.ru>
2023-04-16 13:56:30 +0300

    almost done

--- CRUD_BMI_calc.py
+++ CRUD_BMI_calc.py
@@ -27,11 +27,10 @@ def list_of_users():
         4: "Королевич",
         5: "Сапожник"
     }
-    return (login)
+    return login
 
 
 def users_info():
-
     uinfo = {
         1: {"Рост": "180",
             "Вес": "120",
@@ -51,39 +50,73 @@ def users_info():
     }
     return (uinfo)
 
-
 def edit_users_info():
     uinfo = users_info()
+    login = list_of_users()
     id = int(input("Введите id пользователя, которого вы хотите редактировать: "))
-    print (uinfo[id])
+    print ("Имя: ", login [id])
+    print ("Параметры:", uinfo[id])
     param = input(
-        "Введите параметр, которые вы хотите отредактировать (Рост, Вес, Шкала BMI): ")
+        "Введите параметр, которые вы хотите редактировать (Рост, Вес, Шкала BMI): ")
     value = input("Введите новое значение: ")
     uinfo[id][param] = value
     return uinfo[id]
 
 def deleting_user():
     uinfo = users_info()
-    id_del = int(input("Введите id пользователя, которого вы хотите удалить: "))
-    del uinfo [id_del]
+    id = int(input("Введите id пользователя, которого вы хотите удалить: "))
+    del uinfo [id]
+    return uinfo
+
+def adding_login():
+    login = list_of_users()
+    new_id = int(input ("Введите ваш ID: "))
+    while (new_id in login):
+        print("Введите другой ID, такой уже есть! ")
+        new_id = int(input("Введите ваш ID: "))  
+    new_login = input("Введите ваше имя: ")
+    login[new_id] = new_login
+    return login
+
+def adding_parameters():
+    uinfo = users_info()
+    new_id = int(input ("Введите ваш ID: "))
+    while (new_id in uinfo):
+        print("Введите другой ID, такой уже есть! ")
+        new_id = int(input("Введите ваш ID: ")) 
+    heigh = float(input("Введите Ваш рост в метрах (например - 1.86): "))
+    weight = float(input("Введите Ваш вес в килограммах (например - 67): "))
+    s = [20]+["="]*15+[50] # 20 - minimal BMI, 50 - maximal BMI, one "=" - 2 points
+    bmi = round(weight/(heigh**2), 2)
+    if 20 <= bmi <= 50:
+        i = int(round((bmi-20)/2)) # position "|" in list, where 20 - minimal BMI
+        s[i] = "|"
+    else:
+        s = s
+    uinfo[new_id] = {"Рост": heigh, "Вес": weight, "Шкала BMI": s}
     return uinfo
 
 
 def process_option(option):
-    if option == "W":
+    if option == "W" or option == "1":
         print(list_of_users())
-    elif option == "I":
+    elif option == "I" or option == "2":
         id = int(input("Введите id пользователя: "))
         uinfo = users_info()
         print(uinfo[id])
-    elif option == "E":
+    elif option == "E" or option == "3":
         print("Новые параметры: ", edit_users_info())
-    elif option == "D":
-        print("Оставшиеся пользователи: ", deleting_user())    
+    elif option == "D" or option == "4":
+        print("Оставшиеся пользователи: ", deleting_user())   
+    elif option == "A" or option == "5":
+        new_login = adding_login()
+        new_info = adding_parameters()
+        id = int(input("Введите ID еще раз: "))
+        print ("Ваш логин:", new_login[id])
+        print ("Ваши параметры", new_info[id])
 
 
 def main():
-    # while option != "Q":
     option = print_menu_and_get_option()
     process_option(option)
 
