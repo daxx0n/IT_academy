8762dfebb74b9d5050347c39c9d4076f6ca28557
Stanislav_Ksenzov <daxxon@mail.ru>
2023-04-16 09:33:49 +0300

    edit_users_info added feature

--- CRUD_BMI_calc.py
+++ CRUD_BMI_calc.py
@@ -1,6 +1,11 @@
 import os
 
-def print_and_choice_menu():
+
+def clear_screen():
+    os.system("clear")
+
+
+def print_menu_and_get_option():
     print("Добро пожаловать в калькулятор BMI!")
     print("Что бы вы хотели сделать?")
     print("1. Вывести список пользователей", "(W)")
@@ -9,36 +14,67 @@ def print_and_choice_menu():
     print("4. Удалить выбранного пользователя", "(D)")
     print("5. Добавить пользователя", "(A)")
     print("6. Выход", "(Q)")
-    choice = input ("Введите свой выбор: ")
-    os.system('clear') 
-    return choice
+    option = input("Введите свой выбор: ")
+    clear_screen()
+    return option
+
 
 def list_of_users():
     login = {
-        1:"Царь", 
-        2:"Царевич",
-        3:"Король",
-        4:"Королевич",
-        5:"Сапожник"
-        }
-    return(login) 
-
-def choice_option():
-    if print_and_choice_menu == "W":
-        print(list_of_users())   
+        1: "Царь",
+        2: "Царевич",
+        3: "Король",
+        4: "Королевич",
+        5: "Сапожник"
+    }
+    return (login)
 
 
-def main ():
-    while print_and_choice_menu != "Q":
-        list_of_options = print_and_choice_menu()
-        choice_option(list_of_options)
+def users_info():
+    
+    uinfo = {
+        1: {"Рост":"180",
+            "Вес":"120",
+            "Шкала BMI":"20========|======50"},
+        2: {"Рост":"150",
+            "Вес":"80",
+            "Шкала BMI":"20=======|=======50"},
+        3: {"Рост":"190",
+            "Вес":"54",
+            "Шкала BMI":"20===============50"},
+        4: {"Рост":"177",
+            "Вес":"100",
+            "Шкала BMI":"20=====|=========50"},
+        5: {"Рост":"130",
+            "Вес":"80",
+            "Шкала BMI":"20=============|=50"}
+    }
+    return (uinfo)
+    
+
+def process_option(option):
+    if option == "W":
+        print(list_of_users())
+    elif option == "I":
+        id = int(input("Введите id пользователя: "))
+        uinfo = users_info()
+        print(uinfo[id])        
+    elif option == "E":
+        print (users_edit())
+        
+
+
+def main():
+    #while option != "Q":
+    option = print_menu_and_get_option()
+    process_option(option)
 
 
 main()
 
-#1. многопольз калькулятор БМИ(словарь) на 5 человек, шкалу тоже надо выводить
-#2. Храним рост, вес, пол,
-#3. Меню:CRUD(L)
+# 1. многопольз калькулятор БМИ(словарь) на 5 человек, шкалу тоже надо выводить
+# 2. Храним рост, вес, пол,
+# 3. Меню:CRUD(L)
 #    1. Вывести список польз(ключ - login)(L)
 #    2. Посмотреть инфо о пользователе (рост, вес, шкала БМИ) (R)
 #    3. Изменить данные о пользователе (выбираем соот во ключу) (U)
