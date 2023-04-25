6a48eadfa6095cf9f9eee5e2a003dfac3c0acca2
Stanislav_Ksenzov <daxxon@mail.ru>
2023-04-25 22:24:29 +0300

    some rebuild and adding colour

--- clock.py
+++ clock.py
@@ -1,12 +1,18 @@
 import os
 import time
 from datetime import datetime
+from colorama import init
+init()
+from colorama import Fore, Back, Style
 
-current_time = datetime.now().time()
-str_tn = current_time.strftime("%H:%M:%S")
-
+def string_time():
+    current_time = datetime.now().time()
+    str_tn = current_time.strftime("%H:%M:%S")
+    return str_tn
+    
+    
 def clear_screen():
-    os.system("clear")     
+    os.system("clear")
 
 num_0 = """ 
 ███████
@@ -110,7 +116,7 @@ clock_numbers = {
 }
 
 
-def clock_convert (time_string):
+def clock_convert(time_string):
     num1 = None
     num2 = None
     if len(time_string) == 1:
@@ -119,36 +125,51 @@ def clock_convert (time_string):
     else:
         num1 = clock_numbers[time_string[0]]
         num2 = clock_numbers[time_string[1]]
-    return multilines_convert (num1, num2)
+    return multilines_convert(num1, num2)
 
 
-def multilines_convert (num1, num2):
-    numbers = [f"{num1}  {num2}" for num1, num2 in zip (num1.splitlines(), num2.splitlines())]
+def multilines_convert(num1, num2):
+    numbers = [f"{num1}  {num2}" for num1, num2 in zip(
+        num1.splitlines(), num2.splitlines())]
     res = "\n".join(i for i in numbers)
     return res
 
+def clock():
+     str_time = string_time()
+     hours = clock_convert(str_time[:2])
+     minutes = clock_convert(str_time[3:5])
+     seconds = clock_convert(str_time[6:8])
+     if int(str_time[6:8]) % 2 == 0:
+          clock = multilines_convert(hours, clock_numbers[":"])
+     else:
+          clock = multilines_convert(hours, clock_numbers["_"])
+     clock = multilines_convert(clock, minutes)
+     if int(str_time[6:8]) % 2 == 0:
+          clock = multilines_convert(clock, clock_numbers[":"])
+     else:
+          clock = multilines_convert(clock, clock_numbers["_"])
+     clock = multilines_convert(clock, seconds)
+     return clock
+
+def color_output():
+     str_time = string_time()
+     clock_out = clock()
+     if int(str_time[7]) == 0 or int(str_time[7]) == 1:
+          print(Fore.GREEN + clock_out)
+     if int(str_time[7]) == 2 or int(str_time[7]) == 3:
+          print(Fore.RED + clock_out)
+     if int(str_time[7]) == 4 or int(str_time[7]) == 5:
+          print(Fore.BLUE + clock_out)
+     if int(str_time[7]) == 6 or int(str_time[7]) == 7:
+          print(Fore.YELLOW + clock_out)
+     if int(str_time[7]) == 8 or int(str_time[7]) == 9:
+          print(Fore.MAGENTA+ clock_out)     
 
 def main():
     while True:
         clear_screen()
-        current_time = datetime.now().time()
-        str_tn = current_time.strftime("%H:%M:%S")
-        hours = clock_convert(str_tn[:2])
-        minutes = clock_convert(str_tn[3:5])
-        seconds = clock_convert(str_tn[6:8])
-        
-        if int(str_tn[6:8])%2 == 0:
-            clock = multilines_convert(hours, clock_numbers[":"])
-        else:
-            clock = multilines_convert(hours, clock_numbers["_"])
-        clock = multilines_convert(clock, minutes)
-        if int(str_tn[6:8])%2 == 0:
-            clock = multilines_convert(clock, clock_numbers[":"])
-        else:
-            clock = multilines_convert(clock, clock_numbers["_"])
-        clock = multilines_convert(clock, seconds)  
-        print(clock)
-
+        color_output()
         time.sleep(1)
+
 
 main()
