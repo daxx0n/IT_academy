5c007136b79e24d69b65791dd10170f719ba1c47
Stanislav_Ksenzov <daxxon@mail.ru>
2023-04-25 23:03:13 +0300

    added square top-down feature

--- clock.py
+++ clock.py
@@ -3,16 +3,8 @@ import time
 from datetime import datetime
 from colorama import init
 init()
-from colorama import Fore, Back, Style
+from colorama import Fore
 
-def string_time():
-    current_time = datetime.now().time()
-    str_tn = current_time.strftime("%H:%M:%S")
-    return str_tn
-    
-    
-def clear_screen():
-    os.system("clear")
 
 num_0 = """ 
 ███████
@@ -22,16 +14,16 @@ num_0 = """
 ███████
 """
 num_1 = """
-     ██
-  ██ ██
-     ██
-     ██
-     ██
+   ██  
+█████  
+   ██  
+   ██  
+███████
 """
 num_2 = """
 ███████
     ███
-  ███  
+███████
 ███    
 ███████
 """
@@ -85,19 +77,40 @@ num_9 = """
      ██
 ███████
 """
-colon = """
-  ███
+colon1 = """
   ███
      
-  ███
-  ███
+     
+     
+     
 """
-nothing = """
+colon2 = """
+     
+  ███
+     
+     
+     
+"""
+colon3 = """
+     
+     
+  ███
+     
+     
+"""
+colon4 = """
+     
+     
+     
+  ███
+     
+"""
+colon5 = """
      
      
      
      
-     
+  ███
 """
 
 clock_numbers = {
@@ -111,20 +124,31 @@ clock_numbers = {
     "8": num_8,
     "9": num_9,
     "0": num_0,
-    ":": colon,
-    "_": nothing
+    ":1": colon1,
+    ":2": colon2,
+    ":3": colon3,
+    ":4": colon4,
+    ":5": colon5,
 }
+def clear_screen():
+    os.system("clear")
+    
+    
+def string_time():
+    current_time = datetime.now().time()
+    str_tn = current_time.strftime("%H:%M:%S")
+    return str_tn
+    
 
-
-def clock_convert(time_string):
+def clock_convert(str_time):
     num1 = None
     num2 = None
-    if len(time_string) == 1:
+    if len(str_time) == 1:
         num1 = clock_numbers["0"]
-        num2 = clock_numbers[time_string]
+        num2 = clock_numbers[str_time]
     else:
-        num1 = clock_numbers[time_string[0]]
-        num2 = clock_numbers[time_string[1]]
+        num1 = clock_numbers[str_time[0]]
+        num2 = clock_numbers[str_time[1]]
     return multilines_convert(num1, num2)
 
 
@@ -134,22 +158,38 @@ def multilines_convert(num1, num2):
     res = "\n".join(i for i in numbers)
     return res
 
+
 def clock():
      str_time = string_time()
      hours = clock_convert(str_time[:2])
      minutes = clock_convert(str_time[3:5])
      seconds = clock_convert(str_time[6:8])
-     if int(str_time[6:8]) % 2 == 0:
-          clock = multilines_convert(hours, clock_numbers[":"])
-     else:
-          clock = multilines_convert(hours, clock_numbers["_"])
+     if int(str_time[7]) == 0 or int(str_time[7]) == 9:
+          clock = multilines_convert(hours, clock_numbers[":1"])
+     elif int(str_time[7]) == 1 or int(str_time[7]) == 8:
+          clock = multilines_convert(hours, clock_numbers[":2"])
+     elif int(str_time[7]) == 2 or int(str_time[7]) == 7:
+          clock = multilines_convert(hours, clock_numbers[":3"])
+     elif int(str_time[7]) == 3 or int(str_time[7]) == 6:
+          clock = multilines_convert(hours, clock_numbers[":4"])
+     elif int(str_time[7]) == 4 or int(str_time[7]) == 5:
+          clock = multilines_convert(hours, clock_numbers[":5"])
+    
      clock = multilines_convert(clock, minutes)
-     if int(str_time[6:8]) % 2 == 0:
-          clock = multilines_convert(clock, clock_numbers[":"])
-     else:
-          clock = multilines_convert(clock, clock_numbers["_"])
+     
+     if int(str_time[7]) == 0 or int(str_time[7]) == 9:
+          clock = multilines_convert(clock, clock_numbers[":1"])
+     elif int(str_time[7]) == 1 or int(str_time[7]) == 8:
+          clock = multilines_convert(clock, clock_numbers[":2"])
+     elif int(str_time[7]) == 2 or int(str_time[7]) == 7:
+          clock = multilines_convert(clock, clock_numbers[":3"])
+     elif int(str_time[7]) == 3 or int(str_time[7]) == 6:
+          clock = multilines_convert(clock, clock_numbers[":4"])
+     elif int(str_time[7]) == 4 or int(str_time[7]) == 5:
+          clock = multilines_convert(clock, clock_numbers[":5"])
      clock = multilines_convert(clock, seconds)
      return clock
+
 
 def color_output():
      str_time = string_time()
@@ -164,6 +204,7 @@ def color_output():
           print(Fore.YELLOW + clock_out)
      if int(str_time[7]) == 8 or int(str_time[7]) == 9:
           print(Fore.MAGENTA+ clock_out)     
+
 
 def main():
     while True:
